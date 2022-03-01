30e5e7c989978f5974539434a42bb311d84e5859
raul <raulherbert7@gmail.com>
2022-03-01 15:15:31 -0300

    atualização form

--- js/main.js
+++ js/main.js
@@ -20,3 +20,17 @@
     },
   });
 });
+
+function sendEmail() {
+    Email.send({
+        Host : "smtp.yourisp.com",
+        Username : "username",
+        Password : "password",
+        To : 'them@website.com',
+        From : "you@isp.com",
+        Subject : "This is the subject",
+        Body : "And this is the body"
+    }).then(
+      message => alert(message)
+    );
+}
